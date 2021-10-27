749fec8f7f651525790941d320b3984a9ef38329
Dea Aprizal <43339356+deaaprizal@users.noreply.github.com>
2021-10-27 18:14:49 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    mongodburl: "mongodb+srv://mkuser:IDOd9j0tLULMWTJJ@cluster0.vlntv.mongodb.net/mejakerja_db?retryWrites=true&w=majority",
-    MONGO_DB_NAME: "mejakerja_db",
-  }
-}
