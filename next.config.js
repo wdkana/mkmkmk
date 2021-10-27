5e83f166c083c159f6e9e7779c58f9cd3b943f33
teday <tedi.suryadi77@gmail.com>
2021-10-27 19:11:35 +0700

    generate key

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    mongodburl: "mongodb+srv://mkuser:IDOd9j0tLULMWTJJ@cluster0.vlntv.mongodb.net/mejakerja_db?retryWrites=true&w=majority",
+    MONGO_DB_NAME: "mejakerja_db",
+  }
 }
