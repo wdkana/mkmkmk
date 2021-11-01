5b6c747aba42e254881b507dad2e45834d08fd2c
Tedi Suryadi <tedi.suryadi77@gmail.com>
2021-11-01 18:07:28 +0700

    fix conflict landing page

--- Components/LandingPage/index.js
+++ Components/LandingPage/index.js
@@ -14,10 +14,7 @@ import {
   Alert,
   AlertIcon,
   AlertTitle,
-  Text,
   AlertDescription,
-  AlertDialogFooter,
-  Divider,
   Button,
   Link,
   Modal,
@@ -94,178 +91,120 @@ const LandingPage = (props) => {
         backgroundSize="cover"
         h="100vh"
       >
-        <Box position="relative" top={280}>
-          <Flex p={5} my="auto">
-            <Center w="100%">
-              <Wrap>
-                <WrapItem
-                  boxShadow="lg"
-                  rounded="lg"
-                  w={["100%", "100%", 350, 450, 450, 500]}
-                >
-                  <InputGroup size="lg">
-                    <InputLeftAddon children="mejakerja.com/" />
-                    <Input
-                      type="search"
-                      placeholder="username"
-                      color={mode == "dark" ? "white" : "gray.300"}
-                      onChange={(e) => InputUsername(e.target.value)}
-                    />
-                  </InputGroup>
-                </WrapItem>
-                <WrapItem w={["100%", "100%", 125, 150, 150, 175]}>
-                  <Button
+        <Box bgGradient="linear(to-r, black, gray.900)" h="100vh">
+          <Box position="relative" top={280}>
+            <Flex p={5} my="auto">
+              <Center w="100%">
+                <Wrap>
+                  <WrapItem
                     boxShadow="lg"
-                    w="100%"
-                    size="lg"
-                    isLoading={false}
-                    colorScheme="messenger"
-                    loadingText="Mendaftar"
-                    rightIcon={<FaArrowRight />}
-                    onClick={ onOpen }
+                    rounded="lg"
+                    w={["100%", "100%", 350, 450, 450, 500]}
                   >
-                    Mulai
-                  </Button>
-                </WrapItem>
-              </Wrap>
-            </Center>
-          </Flex>
-          <Flex pl={5} pr={5} my="auto">
-            <Center w="100%">
-              <Alert
-                status="info"
-                w={["100%", 525, 490, 610, 610, 690]}
-                boxShadow="xl"
-                rounded="md"
-                bgColor={ mode == "dark" ? "gray.700" : "whiteAlpha.700"}
-              >
-                <AlertIcon />
-                <AlertTitle mr={2}>mejamu mejaku meja kita semua.</AlertTitle>
-                <AlertDescription whiteSpace="nowrap" w="100%" align="right">
-                  <Link as={ReachLink} to="/explore" ml={1}>
-                    <u>masuk</u>
-                  </Link>
-                  <small> atau </small>
-                  <Link as={ReachLink} to="/explore" mr={1}>
-                    <u>
-                      <i>explore</i>
-                    </u>
-                  </Link>
-                </AlertDescription>
-              </Alert>
-            </Center>
-          </Flex>
-        </Box>
-    <Box bgGradient="linear(to-r, black, gray.900)" h="100vh">
-      <Box position="relative" top={280}>
-        <Flex p={5} my="auto">
-          <Center w="100%">
-            <Wrap>
-              <WrapItem
-                boxShadow="lg"
-                rounded="lg"
-                w={["100%", "100%", 350, 450, 450, 500]}
-              >
-                <InputGroup size="lg">
-                  <InputLeftAddon children="mejakerja.com/" />
-                  <Input
-                    type="search"
-                    placeholder="username"
-                    color="white"
-                  />
-                </InputGroup>
-              </WrapItem>
-              <WrapItem w={["100%", "100%", 125, 150, 150, 175]}>
-                <Button
-                  boxShadow="lg"
-                  w="100%"
-                  size="lg"
-                  isLoading={false}
-                  colorScheme="messenger"
-                  bgColor={mode == "dark" ? "blue.900" : "blue.900"}
-                  color={mode == "dark" ? "whiteAlpha.900" : "whiteAlpha.900"}
-                  loadingText="Mendaftar"
-                  rightIcon={<FaArrowRight />}
+                    <InputGroup size="lg">
+                      <InputLeftAddon children="mejakerja.com/" />
+                      <Input
+                        type="search"
+                        placeholder="username"
+                        color="white"
+                        onChange={ (e) => InputUsername(e.target.value)}
+                      />
+                    </InputGroup>
+                  </WrapItem>
+                  <WrapItem w={["100%", "100%", 125, 150, 150, 175]}>
+                    <Button
+                      boxShadow="lg"
+                      w="100%"
+                      size="lg"
+                      isLoading={false}
+                      colorScheme="messenger"
+                      bgColor={mode == "dark" ? "blue.900" : "blue.900"}
+                      color={mode == "dark" ? "whiteAlpha.900" : "whiteAlpha.900"}
+                      loadingText="Mendaftar"
+                      rightIcon={<FaArrowRight />}
+                      onClick={ onOpen }
+                    >
+                      Mulai
+                    </Button>
+                  </WrapItem>
+                </Wrap>
+              </Center>
+            </Flex>
+            <Flex pl={5} pr={5} my="auto">
+              <Center w="100%">
+                <Alert
+                  status="info"
+                  w={["100%", 525, 490, 610, 610, 690]}
+                  boxShadow="xl"
+                  rounded="md"
+                  bgColor={mode == "dark" ? "whiteAlpha.100" : "whiteAlpha.100"}
+                  color="whiteAlpha.700"
                 >
-                  Mulai
-                </Button>
-              </WrapItem>
-            </Wrap>
-          </Center>
-        </Flex>
-        <Flex pl={5} pr={5} my="auto">
-          <Center w="100%">
-            <Alert
-              status="info"
-              w={["100%", 525, 490, 610, 610, 690]}
-              boxShadow="xl"
-              rounded="md"
-              bgColor={mode == "dark" ? "whiteAlpha.100" : "whiteAlpha.100"}
-              color="whiteAlpha.700"
-            >
-              <AlertIcon />
-              <AlertTitle mr={2}>mejamu mejaku meja kita semua.</AlertTitle>
-              <AlertDescription whiteSpace="nowrap" w="100%" align="right">
-                <Link href="/login" ml={1}>
-                  <u>masuk</u>
-                </Link>
-                <small> atau </small>
-                <Link href="/explore" mr={1}>
-                  <u>
-                    <i>explore</i>
-                  </u>
-                </Link>
-              </AlertDescription>
-            </Alert>
-          </Center>
-        </Flex>
-      </Box>
-      <Modal isOpen={isOpen} onClose={closeModal} size="lg">
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{ props.userReducer.isError ? "Register Gagal" : props.userReducer.private_key_register !== null ? "Simpan key anda" : username.length > 3 ? "Pin" : "Warning" }</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            { props.userReducer.private_key_register !== null
-              ? <UnorderedList>
-              <ListItem>private key: { props.userReducer.private_key_register } 
-                <CopyToClipboard text={ props.userReducer.private_key_register }>
-                  <Button size="xs"><FiCopy /></Button>
-                </CopyToClipboard>
-                <Button size="xs" onClick={ (e) => downloadTxtFile("private",e) }><FiDownload /></Button>
-              </ListItem>
-              <ListItem>public key: { props.userReducer.public_key_register }
-                <CopyToClipboard text={ props.userReducer.public_key_register }>
-                  <Button size="xs"><FiCopy /></Button>
-                </CopyToClipboard>
-                <Button size="xs" onClick={ (e) => downloadTxtFile("public",e) }><FiDownload /></Button>
-              </ListItem>
-             </UnorderedList> : props.userReducer.isError ? "Maaf IP atau username kamu sudah terdaftar" : username.length > 3 ? 
-              <HStack>
-                <PinInput mask otp onChange={ (e) => setPin(e) }>
-                  <PinInputField />
-                  <PinInputField />
-                  <PinInputField />
-                  <PinInputField />
-                  <PinInputField />
-                  <PinInputField />
-                </PinInput>
-              </HStack> : "Username minimal 6 digit"
-            }
-          </ModalBody>
+                  <AlertIcon />
+                  <AlertTitle mr={2}>mejamu mejaku meja kita semua.</AlertTitle>
+                  <AlertDescription whiteSpace="nowrap" w="100%" align="right">
+                    <Link href="/login" ml={1}>
+                      <u>masuk</u>
+                    </Link>
+                    <small> atau </small>
+                    <Link href="/explore" mr={1}>
+                      <u>
+                        <i>explore</i>
+                      </u>
+                    </Link>
+                  </AlertDescription>
+                </Alert>
+              </Center>
+            </Flex>
+          </Box>
+          <Modal isOpen={isOpen} onClose={closeModal} size="lg">
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>{ props.userReducer.isError ? "Register Gagal" : props.userReducer.private_key_register !== null ? "Simpan key anda" : username.length > 3 ? "Pin" : "Warning" }</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                { props.userReducer.private_key_register !== null
+                  ? <UnorderedList>
+                  <ListItem>private key: { props.userReducer.private_key_register } 
+                    <CopyToClipboard text={ props.userReducer.private_key_register }>
+                      <Button size="xs"><FiCopy /></Button>
+                    </CopyToClipboard>
+                    <Button size="xs" onClick={ (e) => downloadTxtFile("private",e) }><FiDownload /></Button>
+                  </ListItem>
+                  <ListItem>public key: { props.userReducer.public_key_register }
+                    <CopyToClipboard text={ props.userReducer.public_key_register }>
+                      <Button size="xs"><FiCopy /></Button>
+                    </CopyToClipboard>
+                    <Button size="xs" onClick={ (e) => downloadTxtFile("public",e) }><FiDownload /></Button>
+                  </ListItem>
+                </UnorderedList> : props.userReducer.isError ? "Maaf IP atau username kamu sudah terdaftar" : username.length > 3 ? 
+                  <HStack>
+                    <PinInput mask otp onChange={ (e) => setPin(e) }>
+                      <PinInputField />
+                      <PinInputField />
+                      <PinInputField />
+                      <PinInputField />
+                      <PinInputField />
+                      <PinInputField />
+                    </PinInput>
+                  </HStack> : "Username minimal 4 digit"
+                }
+              </ModalBody>
 
-          <ModalFooter>
-            { props.userReducer.private_key_register !== null || props.userReducer.isError ? false : username.length > 3 ?
-                <Button isDisabled={ pin.length == 6 ? false : true } isLoading={ props.userReducer.isLoading } colorScheme="blue" mr={3} onClick={Register}>
-                  Masuk
-                </Button> :
-                <Button colorScheme="blue" mr={3} onClick={onClose}>
-                  Tutup
-                </Button>
-            }
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+              <ModalFooter>
+                { props.userReducer.private_key_register !== null || props.userReducer.isError ? false : username.length > 3 ?
+                    <Button isDisabled={ pin.length == 6 ? false : true } isLoading={ props.userReducer.isLoading } colorScheme="blue" mr={3} onClick={Register}>
+                      Masuk
+                    </Button> :
+                    <Button colorScheme="blue" mr={3} onClick={onClose}>
+                      Tutup
+                    </Button>
+                }
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </Box>
+      </Box>
     </React.Fragment>
   );
 }
