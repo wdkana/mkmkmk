dea14169f7476ed83cb7e34c8d60a4ead0390ade
Ratama Adhi Nugraha <ratama.adhi@gmail.com>
2021-11-03 10:51:32 +0700

    fix(username): add layout -build

---
+++ pages/[username]/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../Components/Layout/Layout';
+
+function IndexUsername() {
+  return (
+    <Layout>
+    </Layout>
+  );
+}
+
+export default IndexUsername
